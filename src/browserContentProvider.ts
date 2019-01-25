cfd153aaec077a53556584f29e8e4d97f26b2856
nexcvon <1702313+nexcvon@users.noreply.github.com>
2019-01-25 11:23:12 +0800

    improve preview layout

--- src/browserContentProvider.ts
+++ src/browserContentProvider.ts
@@ -23,7 +23,7 @@ export class BrowserContentProvider implements vscode.TextDocumentContentProvide
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>Preview</title>
-            <style>iframe { background-color: white } </style>
+            <style>iframe { position: absolute; right: 0; bottom: 0; left: 0; top: 0; border: 0; background-color: white } </style>
         </head>
         <body>
             <iframe src="${uri}" frameBorder="0" width="100%" height="100%" />
